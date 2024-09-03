c2eeda7145c9a97577b289de61fad2e23a635fa8
guilhermeSantos4701 <guilhermesantos4701@gmail.com>
2024-09-03 20:40:31 -0300

    Criação do banco de dados da Atividade Avaliativa

---
+++ create-database.mongodb.js
@@ -0,0 +1,10 @@
+/* NOME DO BANCO DE DADOS */
+const database = 'BD3-GUILHERMEJESUS-AULA';
+
+/* NOME DO BANCO DA COLEÇÃO DE DADOS */
+const collection = 'ALUNOS';
+
+/* CRIAR OU ACESSAR BANCO DE DADOS: */
+use(database);
+
+db.createCollection(collection)
